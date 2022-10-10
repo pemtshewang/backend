f1682189cd5798c768b7171ae33fb51e5ec1ab65
PemTshewang <77913595+pemtshewang@users.noreply.github.com>
2022-10-10 18:37:51 +0600

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -168,8 +168,6 @@ CORS_ALLOWED_ORIGINS = [
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587 
-EMAIL_HOST_USER = 'iamgettingbetter777@gmail.com'
-EMAIL_HOST_PASSWORD = '$7Bpemtshewang'
 EMAIL_USE_TLS = True
 ########################################################
 
